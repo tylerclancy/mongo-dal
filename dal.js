389ebbaf9b3a1e1d39afb60f644a4b12f5ba73db
Tyler <tylerclancy030@gmail.com>
2022-03-22 15:46:42 -0500

    Starter files

---
+++ dal.js
@@ -0,0 +1,65 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+let db = null;
+
+// connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+    console.log("Connected successfully to db server");
+
+    // connect to myproject database
+    db = client.db('myproject');
+});
+
+// create user account using the collection.insertOne function
+function create(name, email, password) {
+    // TODO: populate this function based off the video
+}
+
+// find user account 
+function find(email) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .find({ email: email })
+            .toArray(function (err, docs) {
+                err ? reject(err) : resolve(docs);
+            });
+    })
+}
+
+// find user account
+function findOne(email) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOne({ email: email })
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOneAndUpdate(
+                { email: email },
+                { $inc: { balance: amount } },
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );
+
+
+    });
+}
+
+// return all users by using the collection.find method
+function all() {
+    // TODO: populate this function based off the video
+}
+
+
+module.exports = { create, findOne, find, update, all };
